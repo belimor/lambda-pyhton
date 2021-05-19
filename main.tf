c308d75a6e8dc57e0555c872d5af34103d8fcec5
dmitry <dmitry.vaghin@gmail.com>
2021-05-19 00:43:55 -0600

    update

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "Hello-World-Lambda" {
+  source        = "modules/lambda"
+
+  function_name = "HelloWorld"
+  description   = "Hello World Lambda Function"
+  aws_project   = var.aws_project
+  project_tags  = var.project_tags
+}
